a7074ff30452f00656189ec581b3332ec88b1e03
waveyl <1038412237@qq.com>
2023-03-07 15:44:47 +0800

    Update 翻译.js

--- 翻译.js
+++ 翻译.js
@@ -222,7 +222,7 @@ function tryTranslation(value) {
     },
     { retries: 1, maxTimeout: 10000, randomize: true }
   ).catch((error) => {
-    const errorMessage = `${TRANSLATION_ERROR}1: ${error.?message} ${error.stack}\nresult:\n${lastResult}\nFrom:\n${value}\nstringToTranslate:\n${stringToTranslate}\nRetryCount: ${retryCount}\nRetry Again\n--\n\n `;
+    const errorMessage = `${TRANSLATION_ERROR}1: ${error?.message} ${error?.stack}\nresult:\n${lastResult}\nFrom:\n${value}\nstringToTranslate:\n${stringToTranslate}\nRetryCount: ${retryCount}\nRetry Again\n--\n\n `;
     logger.error(errorMessage);
     return TRANSLATION_ERROR;
   });
